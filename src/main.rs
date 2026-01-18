9476dfa689b0de13fc3bc7e8534aa36c185a0bf7
calebesouzas <cacalebehs@gmail.com>
2026-01-18 17:42:46 -0300

    feat(lexer): read content from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+
+
+enum TokenType {
+    NumberLiteral,
+    BinaryOperator,
+    Identifier,
+    OpenParen,
+    CloseParen,
+    EOF
+}
+
+struct Token {
+    kind: TokenType,
+    value: String
+}
+
+fn tokenize(source_code: &String) {
+    return;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Raiz - version 0.1-dev");
+    println!("Use 'quit()' function to exit...");
+    let mut code = String::new();
+    loop {
+        print!(">> ");
+        io::stdin()
+            .read_line(&mut code)
+            .expect("Failed to read line");
+        println!("{}", code);
+    }
 }
