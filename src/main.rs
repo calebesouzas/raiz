1fde0400c6508e76bee2a16d40d93800947b62a2
calebesouzas <cacalebehs@gmail.com>
2026-01-19 21:25:15 -0300

    Remove everything but args vector

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,7 @@
-use std::io;
-
-
-enum TokenType {
-    NumberLiteral,
-    BinaryOperator,
-    Identifier,
-    OpenParen,
-    CloseParen,
-    EOF
-}
-
-struct Token {
-    kind: TokenType,
-    value: String
-}
-
-fn tokenize(source_code: &String) {
-    return;
-}
+use std::env;
 
 fn main() {
     println!("Raiz - version 0.1-dev");
-    println!("Use 'quit()' function to exit...");
-    let mut code = String::new();
-    loop {
-        print!(">> ");
-        io::stdin()
-            .read_line(&mut code)
-            .expect("Failed to read line");
-        println!("{}", code);
-    }
+
+    let _args = env::args;
 }
