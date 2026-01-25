f0091a263deb87e409defabd486f0a734a1ec5fc
calebesouzas <cacalebehs@gmail.com>
2026-01-25 14:11:47 -0300

    tests(lexer): add non-empty token vec and change negative numbers test

--- src/tests.rs
+++ src/tests.rs
@@ -1,10 +1,20 @@
 use crate::lexer::{Lexer, Token};
 
 #[test]
+fn not_empty() {
+    let code = String::from("10 + 5 - 7");
+    let mut lexer = Lexer::new(&code);
+    lexer.tokenize();
+
+    assert!(!lexer.tokens.is_empty());
+}
+
+#[test]
 fn negative_number_literals() {
     let code = String::from("-15");
     let mut lexer = Lexer::new(&code);
     lexer.tokenize();
 
-    assert_eq!(lexer.tokens[0], Token::NumberLiteral(-15));
+    assert_eq!(lexer.tokens[0], Token::Minus);
+    assert_eq!(lexer.tokens[1], Token::NumberLiteral(15));
 }
