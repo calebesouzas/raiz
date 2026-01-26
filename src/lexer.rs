b87f4aecfdd2ed4badefa419538ef3dba0206e0e
calebesouzas <cacalebehs@gmail.com>
2026-01-25 22:08:30 -0300

    bug(lexer)!: commit before lexer refactor

--- src/lexer.rs
+++ src/lexer.rs
@@ -11,18 +11,21 @@ pub enum Token {
 }
 
 #[derive(Debug)]
-pub struct Lexer<'a> {
-    // input: &'a String,
-    bytes: &'a [u8],
+pub struct Lexer {
+    chars: Vec<char>,
     current: usize,
     pub tokens: Vec<Token>,
 }
 
-impl<'a> Lexer<'a> {
+impl<'a> Lexer {
     pub fn new(input: &'a String) -> Self {
+        let mut chars = Vec::<char>::new();
+        let mut iter = input.chars().into_iter();
+        while let Some(ch) = iter.next() {
+            chars.push(ch);
+        }
         Self {
-            // input,
-            bytes: input.as_bytes(),
+            chars,
             current: 0,
             tokens: Vec::<Token>::new(),
         }
@@ -31,7 +34,7 @@ impl<'a> Lexer<'a> {
         self.tokens.last()
     }
     fn next(&mut self) -> Option<Token> {
-        let c = self.bytes[self.current] as char;
+        let c = self.chars[self.current];
         let result = match c {
             '+' => Some(Token::Plus),
             '-' => Some(Token::Minus),
@@ -41,42 +44,53 @@ impl<'a> Lexer<'a> {
             ')' => Some(Token::CloseParen),
             '0'..='9' => {
                 let number = self.get_number();
-                // -1 so we don't skip one character
-                // right after the end of the number
-                self.current -= 1;
                 Some(Token::NumberLiteral(number))
             }
+            ' ' | '\n' | '\t' | '\r' => None,
             other => {
-                println!("Invalid character: {}", other);
-                None
+                panic!("Invalid character: {}", other);
             }
         };
-        self.current += 1;
         result
     }
+    fn advance(&mut self, amount: usize) {
+        if self.current + amount < self.chars.len() {
+            self.current += amount;
+        }
+    }
     pub fn tokenize(&mut self) {
-        while self.current < self.bytes.len() {
-            if self.peek() != Some(&Token::EndOfFile) {
-                let token = self.next().unwrap_or(Token::EndOfFile);
-
+        while self.current < self.chars.len() {
+            if let Some(token) = self.next() {
                 self.tokens.push(token);
-            } else {
-                break;
+                self.advance(1);
+            } else if self.chars[self.current].is_ascii_whitespace() {
+                self.advance(1);
             }
         }
+        self.tokens.push(Token::EndOfFile);
     }
     fn get_number(&mut self) -> i32 {
         let mut number: i32 = 0;
-        while self.current < self.bytes.len() {
-            let c = self.bytes[self.current] as char;
-            if c.is_ascii_digit() {
-                // 48 is the decimal code for '0'
-                number = number * 10 + (c as i32 - 48);
-                self.current += 1;
-            } else {
-                break;
-            }
+        // Pass a reference for 'chars' starting at the current one
+        let cs = &self.chars[self.current..];
+
+        let mut iter = cs.iter().take_while(|c| c.is_ascii_digit());
+
+        while let Some(c) = iter.next() {
+            number = number * 10 + (*c as i32 - 48);
         }
+        self.advance(iter.count());
+        // while self.current < self.chars.len() {
+        //     let current = self.chars[self.current];
+        //     if current.is_ascii_digit() {
+        //         // 48 is the decimal code for '0'
+        //         number = number * 10 + (current as i32 - 48);
+        //         self.advance(1);
+        //     } else {
+        //         break;
+        //     }
+        // }
+        // self.current -= 1;
         number
     }
 }
