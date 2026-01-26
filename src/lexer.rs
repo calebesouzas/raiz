5b87a1acd2a90432f705111d1b0e3126244d6450
calebesouzas <cacalebehs@gmail.com>
2026-01-26 12:30:40 -0300

    fix(lexer): issue where first char in a number is skipped
    Now it is REALLY fixed without breaking everything else

--- src/lexer.rs
+++ src/lexer.rs
@@ -13,7 +13,6 @@ pub enum Token {
 #[derive(Debug)]
 pub struct Lexer {
     chars: Vec<char>,
-    current: usize,
     pub tokens: Vec<Token>,
 }
 
@@ -26,71 +25,40 @@ impl<'a> Lexer {
         }
         Self {
             chars,
-            current: 0,
             tokens: Vec::<Token>::new(),
         }
     }
-    fn peek(&self) -> Option<&Token> {
-        self.tokens.last()
-    }
-    fn next(&mut self) -> Option<Token> {
-        let c = self.chars[self.current];
-        let result = match c {
-            '+' => Some(Token::Plus),
-            '-' => Some(Token::Minus),
-            '*' => Some(Token::Star),
-            '/' => Some(Token::Slash),
-            '(' => Some(Token::OpenParen),
-            ')' => Some(Token::CloseParen),
-            '0'..='9' => {
-                let number = self.get_number();
-                Some(Token::NumberLiteral(number))
-            }
-            ' ' | '\n' | '\t' | '\r' => None,
-            other => {
-                panic!("Invalid character: {}", other);
-            }
-        };
-        result
-    }
-    fn advance(&mut self, amount: usize) {
-        if self.current + amount < self.chars.len() {
-            self.current += amount;
-        }
-    }
     pub fn tokenize(&mut self) {
-        while self.current < self.chars.len() {
-            if let Some(token) = self.next() {
+        let mut chars = self.chars.iter();
+        while let Some(c) = chars.next() {
+            #[allow(unused_assignments)]
+            let mut token = Token::EndOfFile;
+            match c {
+                '+' => token = Token::Plus,
+                '-' => token = Token::Minus,
+                '*' => token = Token::Star,
+                '/' => token = Token::Slash,
+                '(' => token = Token::OpenParen,
+                ')' => token = Token::CloseParen,
+                '0'..='9' => {
+                    let mut number = *c as i32 - 48;
+                    while let Some(ch) = chars.next()
+                        && ch.is_ascii_digit()
+                    {
+                        number = number * 10 + (*ch as i32 - 48);
+                    }
+                    token = Token::NumberLiteral(number);
+                }
+                other => {
+                    if !other.is_ascii_whitespace() {
+                        panic!("Invalid character: {}", other);
+                    }
+                }
+            }
+            if token != Token::EndOfFile {
                 self.tokens.push(token);
-                self.advance(1);
-            } else if self.chars[self.current].is_ascii_whitespace() {
-                self.advance(1);
             }
         }
         self.tokens.push(Token::EndOfFile);
-    }
-    fn get_number(&mut self) -> i32 {
-        let mut number: i32 = 0;
-        // Pass a reference for 'chars' starting at the current one
-        let cs = &self.chars[self.current..];
-
-        let mut iter = cs.iter().take_while(|c| c.is_ascii_digit());
-
-        while let Some(c) = iter.next() {
-            number = number * 10 + (*c as i32 - 48);
-        }
-        self.advance(iter.count());
-        // while self.current < self.chars.len() {
-        //     let current = self.chars[self.current];
-        //     if current.is_ascii_digit() {
-        //         // 48 is the decimal code for '0'
-        //         number = number * 10 + (current as i32 - 48);
-        //         self.advance(1);
-        //     } else {
-        //         break;
-        //     }
-        // }
-        // self.current -= 1;
-        number
     }
 }
