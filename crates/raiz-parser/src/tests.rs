306a3802392fa113b0feff864ddd7dcbfc7ed04e
calebesouzas <cacalebehs@gmail.com>
2026-02-08 18:43:57 -0300

    fix(tests): rename 'Program' imports to 'RaizProgram'

--- crates/raiz-parser/src/tests.rs
+++ crates/raiz-parser/src/tests.rs
@@ -1,9 +1,10 @@
-use crate::{Program, ProgramParser};
+use crate::ProgramParser;
+use raiz_core::RaizProgram;
 
 #[test]
 fn invalid_character() {
     let code = "ç".to_string();
-    let mut p = Program::new(&code);
+    let mut p = RaizProgram::new(code);
     p.build_ast();
 }
 
@@ -11,6 +12,6 @@ fn invalid_character() {
 fn print_tokens_vec() {
     let code = "var num = 15!".to_string();
 
-    let mut p = Program::new(&code);
+    let mut p = RaizProgram::new(code);
     p.build_ast();
 }
