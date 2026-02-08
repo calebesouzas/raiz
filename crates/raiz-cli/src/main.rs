d3b80f0d5cfe569abd625ee21d44788102484717
calebesouzas <cacalebehs@gmail.com>
2026-02-08 15:26:10 -0300

    tests(cli): replace one-plus-one with var-declaration

--- crates/raiz-cli/src/main.rs
+++ crates/raiz-cli/src/main.rs
@@ -1,42 +1,31 @@
 use std::env;
 
+use raiz_core::Program;
+use raiz_parser::ProgramParser;
+
 fn main() {
     let version = "v0.0.1-test";
     let args = env::args();
 
+    // only the program name
     if args.len() == 1 {
         println!("Raiz language - {}", version);
     }
-    grouping_test();
+
+    // unexpected_char();
+
+    var_dec_test();
 }
-#[allow(unused)]
-fn one_plus_one_test() {
-    let code = String::from("1 + 1");
-    let mut lexer = Lexer::new(&code);
-    lexer.tokenize();
 
-    let mut parser = Parser::new(lexer);
-    let actual_result = parser.parse();
-
-    let wanted_result = Expr::Binary {
-        left: Box::new(Expr::Literal(Value::Int(1))),
-        operator: Operator::Sum,
-        right: Box::new(Expr::Literal(Value::Int(1))),
-    };
-    dbg!(&actual_result);
-    assert_eq!(actual_result, wanted_result);
+fn unexpected_char() {
+    let code = "ç".to_string();
+    let mut p = Program::new(&code);
+    p.build_ast();
 }
-fn grouping_test() {
-    println!("At grouping test!");
-    let code = "15 * ((2 + 7) - 8 / 2)";
-    println!("Code: {}", code);
-    let mut lexer = Lexer::new(&code.to_string());
-    lexer.tokenize();
 
-    println!("Tokens: {:?}", lexer.tokens);
+fn var_dec_test() {
+    let code = "var x = 10!".to_string();
+    let mut p = Program::new(&code);
 
-    let mut parser = Parser::new(lexer);
-    let result = parser.parse();
-
-    dbg!(result);
+    p.build_ast();
 }
