a5d7e9c9adae124f8af4eca822c4d48583344dbb
calebesouzas <cacalebehs@gmail.com>
2026-02-14 09:55:19 -0300

    style(build-script): add some indentation in the messages

--- env.zsh
+++ env.zsh
@@ -2,20 +2,23 @@
 
 function compile_db ()
 {
+  echo "\n  Removing old 'compile_commands.json..."
   rm -f compile_commands.json
   echo "[" > compile_commands.json
   directory="$PWD"
   first=true
+  echo "    Files:"
   for file in $(find src -type f -name "*.c")
   do
     if [[ ! first ]]; then
       
       echo "," >> compile_commands.json
     fi
+    echo "      $file"
     echo "{\"directory\":\"$directory\","\
       "\"file\":\"$file\","\
       "\"command\":\"gcc -c $file -o build/${file/src/obj}.o" \
-      "-Werror -Wall -Wpedantic -Iinclude -g\"}"\
+      "-Werror -Wall -Wextra -Wpedantic -Iinclude -g\"}"\
       >> compile_commands.json
     first=false
   done
@@ -30,29 +33,25 @@ function start ()
 
 function build ()
 {
-  # echo "Generating dirs at 'build/obj'..."
-  # for DIR in $(find src -type d -wholename "src/*")
-  # do
-  #   NEW_DIR=${DIR/src/obj}
-  #   mkdir -p ./build/$NEW_DIR
-  # done
-  echo "Removing old build folder..."
+  echo "  Removing old build folder..."
   rm -rf build
   mkdir -p build build/obj build/bin
-  echo "Compiling files..."
+  echo "\n  Compiling files..."
   for FILE in $(find src -type f -name "*.c")
   do
     NEW_PATH="build/${FILE/src/obj}.o"
-    echo "  Compiling $FILE into $NEW_PATH"
+    echo "    Compiling $FILE into $NEW_PATH"
     gcc -c $FILE -o $NEW_PATH \
-      -Werror -Wall -Wpedantic -Iinclude -g
+      -Werror -Wall -Wextra -Wpedantic -Iinclude -g
   done
-  echo "Building final binary..."
+  echo "\n  Building final binary..."
   OBJECTS=$(find build/obj -type f -name "*.c.o")
-  echo "Objects:"
+  echo "    Objects:"
   for OBJECT in $OBJECTS
   do
-    echo "  $OBJECT"
+    echo "      $OBJECT"
   done
+  echo "\n  Built program at build/bin/raiz"
   gcc $OBJECTS -o build/bin/raiz
+  compile_db
 }
