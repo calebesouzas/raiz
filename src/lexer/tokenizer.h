f623604ed825098b572ef84c04ef1dd67cffed55
calebesouzas <cacalebehs@gmail.com>
2026-02-26 16:06:45 -0300

    feat(lexer): declare tokenizer function

--- src/lexer/tokenizer.h
+++ src/lexer/tokenizer.h
@@ -2,11 +2,10 @@
 #define RAIZ_TOKENIZER
 
 #include "../errors.h"
-#include "../raiz_strings.h"
+#include "../core/strings.h"
 #include "tokens.h"
 
-RZ_DEF_RESULT(Array_Token, String);
-// TODO: declare and define 'tokenizer' function
-
+// TODO: define 'tokenizer' function
+int raiz_tokenize(char* const source_code);
 
 #endif /* ifndef RAIZ_TOKENIZER */
