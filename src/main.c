d0555dc42d7f3887fc98f6c99ed7c1c6a96b52cd
calebesouzas <cacalebehs@gmail.com>
2026-02-27 15:46:34 -0300

    fix: int to unsigned int at main.c

--- src/main.c
+++ src/main.c
@@ -69,8 +69,8 @@ main(int argc, char *argv[]) {
       source_code_buffer[i] = fgetc(p_file);
     }
 
-    Token* tokens = raiz_tokenize(source_code);
-    for (int i = 0; i < array_len(tokens); ++i) {
+    Token* tokens = raiz_tokenize(source_code_buffer);
+    for (unsigned int i = 0; i < array_len(tokens); ++i) {
       if (tokens[i].value != NULL) {
 	free(tokens[i].value);
       }
