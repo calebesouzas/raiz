7a83faa7d9833a86b6f690da0996a5ed6a4b1aba
calebesouzas <cacalebehs@gmail.com>
2026-03-03 12:39:53 -0300

    style(lexer): indent token kind enum

--- src/lexer/tokens.h
+++ src/lexer/tokens.h
@@ -3,10 +3,10 @@
 
 // TODO: add the remaining comments for each TOKEN_<VARIANT>
 typedef enum {
-#define DEF_TOK(variant, string) variant,
-#include "tokens.def"
-#undef DEF_TOK
-  RZ_TK_COUNT
+  #define RAIZ_DEFINE_TOKEN(variant, string) variant,
+  #include "tokens.def"
+  #undef RAIZ_DEFINE_TOKEN
+  RAIZ_TOKEN_COUNT
 } TokenKind;
 
 typedef struct {
