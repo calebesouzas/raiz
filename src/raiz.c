7ea5bd2f8b2e00d1ce8f9fef906cd9b9aa534955
Calebe Souza <cacalebehs@gmail.com>
2026-03-16 22:05:14 -0300

    lexer(errors): implement error messages
    Now we have a little bit of 'invalid character' handling, it is not very strong because it doesn't support UTF-8 or UTF-16

--- src/raiz.c
+++ src/raiz.c
@@ -1,10 +1,20 @@
 #include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#define RAIZ_ERROR_BUFFER_CAPACITY (1024)
+// NOTE: i'm not sure if using a global buffer to store error messages is a good
+// idea. But i think it is (because we don't need to pass a pointer 
+// to in in every single function that can fail for some reason)
+char g_raiz_error_buffer[RAIZ_ERROR_BUFFER_CAPACITY] = {0};
 
 typedef struct {
   enum {
-    TOKEN_EOF,
+    // in case of unrecognized byte or invalid character in current context
+    TOKEN_ERROR,
     TOKEN_PLUS,
     TOKEN_LITERAL_NUMBER,
+    TOKEN_END_OF_FILE
   } kind;
   int value;
 } Token;
@@ -15,13 +25,17 @@ typedef struct {
   char* start;
 } Lexer;
 
-inline void lexer_backup(Lexer* l) {
+static inline Lexer lexer_init(char*const source) {
+  return (Lexer) { .source = source, .current = source, .start = source };
+}
+
+static inline void lexer_backup(Lexer* l) {
   l->start = l->current;
 }
 
 // TODO: add checking for equality between a 'char expected' param and the
 // current character, if not equal, panic on that (in debug version only)!
-inline void lexer_advance(Lexer* l) {
+static inline void lexer_advance(Lexer* l) {
   l->current++;
 }
 
@@ -44,9 +58,16 @@ Token lexer_next_token(Lexer* l) {
       lexer_advance(l);
     }
     return (Token) {.kind = TOKEN_LITERAL_NUMBER, .value = number};
+  default:
+    if (g_raiz_error_buffer[0] != '\0') // means it is not empty
+      memset(g_raiz_error_buffer, 0, RAIZ_ERROR_BUFFER_CAPACITY);
+
+    // Here we are sure that the buffer will not overflow
+    sprintf(g_raiz_error_buffer, "unrecognized character: %c", *l->current);
+    return (Token) {.kind = TOKEN_ERROR };
   }
 
-  return (Token) {.kind = TOKEN_EOF};
+  return (Token) {.kind = TOKEN_END_OF_FILE};
 }
 
 int main(void) {
