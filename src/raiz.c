d7907b5e4d8a0e5d7c06b4de92c0e482a7de182d
Calebe Souza <cacalebehs@gmail.com>
2026-03-17 16:08:12 -0300

    feat: parser + lazy lexer and evaluator
    KNOWN ISSUE: repeated digits after second character in number literals
    are being skipped

--- src/raiz.c
+++ src/raiz.c
@@ -2,8 +2,10 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+//////// DEBUGING ////////
 #define DEBUG
 
 #ifdef DEBUG
@@ -16,12 +18,16 @@
 #define LOG(...)
 #endif
 
+//////// ERRORS ////////
 #define RAIZ_ERROR_BUFFER_CAPACITY (1024)
 // NOTE: i'm not sure if using a global buffer to store error messages is a good
 // idea. But i think it is (because we don't need to pass a pointer 
 // to in in every single function that can fail for some reason)
 char g_raiz_error_buffer[RAIZ_ERROR_BUFFER_CAPACITY] = {0};
 
+#define PANIC(...) do { fprintf(stderr, __VA_ARGS__); exit(1); } while (0)
+
+//////// LEXER (types) ////////
 typedef enum {
   // in case of unrecognized byte or invalid character in current context
   TOKEN_ERROR,
@@ -39,8 +45,10 @@ typedef struct {
   const char* source;
   char* current;
   char* start;
+  size_t len;
 } Lexer;
 
+//////// LEXER (functions) ////////
 static inline Lexer lexer_init(char*const source) {
   return (Lexer) { .source = source, .current = source, .start = source };
 }
@@ -49,10 +57,28 @@ static inline void lexer_backup(Lexer* l) {
   l->start = l->current;
 }
 
+struct LexerAdvance_opt {
+  size_t amount;
+  char expected;
+};
+
+// HACK: optional parameters!
+#define lexer_advance(l, ...)\
+  lexer_advance_opt(l, (struct LexerAdvance_opt) { __VA_ARGS__ })
+
 // TODO: add checking for equality between a 'char expected' param and the
 // current character, if not equal, panic on that (in debug version only)!
-static inline void lexer_advance(Lexer* l) {
-  l->current++;
+static inline void lexer_advance_opt(Lexer* l, struct LexerAdvance_opt opt) {
+  if (opt.expected) assert(*l->current == opt.expected);
+
+  if (!opt.amount) {
+    l->current++;
+    l->len++;
+  }
+  else {
+    l->current += opt.amount;
+    l->len += opt.amount;
+  }
 }
 
 static inline Token lexer_make_token(Lexer* l, TokenKind kind) {
@@ -68,7 +94,6 @@ Token lexer_next_token(Lexer* l) {
   if (l->current == NULL || *l->current == '\0')
     return lexer_make_token(l, TOKEN_END_OF_FILE);
 
-  LOG("Current char: '%c'\n", *l->current);
   switch (*l->current) {
   case ' ':
     while (is_white_space(*l->current)) lexer_advance(l);
@@ -88,11 +113,10 @@ Token lexer_next_token(Lexer* l) {
     lexer_backup(l);
     int number = *l->current - '0';
     while (isdigit(*(++l->current))) {
-      LOG("Current char: '%c'\n", *l->current);
       number = (number * 10) + (*l->current - '0');
       lexer_advance(l);
     }
-    l->current--; // don't skip a char after the number
+    LOG("Found number: %d\n", number);
 
     return (Token) {.kind = TOKEN_LITERAL_NUMBER, .value = number};
   default:
@@ -107,22 +131,102 @@ Token lexer_next_token(Lexer* l) {
   return lexer_make_token(l, TOKEN_END_OF_FILE);
 }
 
-int main(void) {
-  char code[] = "2 + 2";
-  Lexer lexer = lexer_init(code);
+//////// PARSER (types) ////////
+typedef enum {
+  EXPR_BINARY,
+  EXPR_LITERAL,
+} ExprKind;
 
-  Token next;
-  while ((next = lexer_next_token(&lexer)).kind != TOKEN_END_OF_FILE) {
-    if (next.kind == TOKEN_ERROR) {
-      LOG("%s\n", g_raiz_error_buffer);
-      return 1;
+typedef struct Expr Expr;
+struct Expr {
+  ExprKind kind;
+  struct Expr* lhs;
+  struct Expr* rhs;
+  int value; // if kind == LITERAL
+};
+
+typedef struct {
+  Lexer *lex;
+  Expr *ast; // one single for now
+} Parser;
+
+//////// PARSER (functions) ////////
+static inline Parser parser_init(Lexer* lex) {
+  Expr* ast = malloc(sizeof(*ast));
+  assert(ast != NULL);
+  return (Parser) {.lex = lex, .ast = ast};
+}
+
+// TODO: finish handler for simple '+' operations and number literals
+// TODO: free() nodes after use!
+bool parser_next_node(Parser* p, Expr* lhs) {
+  Token tok = lexer_next_token(p->lex);
+  switch (tok.kind) {
+  case TOKEN_ERROR:
+    PANIC("%s\n", g_raiz_error_buffer);
+  case TOKEN_PLUS:
+    lexer_advance(p->lex); // consume '+'
+    Token next_tok = lexer_next_token(p->lex);
+    if (next_tok.kind != TOKEN_LITERAL_NUMBER) PANIC("expected number");
+
+    Expr* rhs = malloc(sizeof(*rhs));
+    if (rhs) {
+      rhs->kind = EXPR_LITERAL;
+      rhs->value = next_tok.value;
     }
 
-    printf("Kind ID: %d", next.kind);
-    if (next.kind == TOKEN_LITERAL_NUMBER) printf(", value: %d", next.value);
-    printf("\n");
-    lexer_advance(&lexer);
+    // Save current state of AST into left side so we don't lose it
+    Expr* lhs = malloc(sizeof(*lhs));
+    assert(lhs != NULL);
+    memcpy(lhs, p->ast, sizeof(*p->ast));
+
+    p->ast->kind = EXPR_BINARY;
+    p->ast->rhs = rhs;
+    return true;
+  case TOKEN_LITERAL_NUMBER:
+    if (!p->ast->lhs) p->ast->lhs = malloc(sizeof(*p->ast));
+    if (p->ast->lhs)
+      (*p->ast->lhs) = (Expr) {.kind = EXPR_LITERAL, .value = tok.value};
+
+    return true;
+  case TOKEN_END_OF_FILE: return false;
   }
+}
+
+Expr* parser_parse(Lexer* l) {
+  Parser *p = malloc(sizeof(*p));
+  assert(p != NULL);
+  *p = parser_init(l);
+
+  // HACK: used this crazy loop to track END_OF_FILE
+  while (parser_next_node(p, NULL));
+
+  return p->ast;
+}
+
+//////// EVALUATOR (functions) ////////
+int eval(Expr* node) {
+  if (node->kind == EXPR_BINARY) {
+    int lhs = eval(node->lhs);
+    int rhs = eval(node->rhs);
+    return lhs + rhs;
+  }
+
+  // if a literal expression:
+  return node->value;
+}
+
+int main(void) {
+  char code[] = "69 + 44 + 1337";
+  Lexer lexer = lexer_init(code);
+
+  Expr* ast = parser_parse(&lexer);
+
+  assert(ast->kind == EXPR_BINARY);
+  assert(ast->lhs->kind == EXPR_LITERAL);
+  assert(ast->rhs->kind == EXPR_LITERAL);
+
+  printf("Result: %d\n", eval(ast));
 
   return 0;
 }
