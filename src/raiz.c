830e67709d22b2c7a0b225cabd8d89fcede6fab4
Calebe Souza <cacalebehs@gmail.com>
2026-03-17 09:51:20 -0300

    fix(lexer): off by one and null character errors
    It was panic() ing at '\0' character and was skipping character after number literals. I also implemented some helper functions but it's not a big deal

--- src/raiz.c
+++ src/raiz.c
@@ -1,6 +1,20 @@
+#include <assert.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+
+#define DEBUG
+
+#ifdef DEBUG
+#define log_file stderr
+
+#define LOG(...)\
+  fprintf(log_file, __VA_ARGS__)
+
+#else
+#define LOG(...)
+#endif
 
 #define RAIZ_ERROR_BUFFER_CAPACITY (1024)
 // NOTE: i'm not sure if using a global buffer to store error messages is a good
@@ -8,14 +22,16 @@
 // to in in every single function that can fail for some reason)
 char g_raiz_error_buffer[RAIZ_ERROR_BUFFER_CAPACITY] = {0};
 
+typedef enum {
+  // in case of unrecognized byte or invalid character in current context
+  TOKEN_ERROR,
+  TOKEN_PLUS,
+  TOKEN_LITERAL_NUMBER,
+  TOKEN_END_OF_FILE
+} TokenKind;
+
 typedef struct {
-  enum {
-    // in case of unrecognized byte or invalid character in current context
-    TOKEN_ERROR,
-    TOKEN_PLUS,
-    TOKEN_LITERAL_NUMBER,
-    TOKEN_END_OF_FILE
-  } kind;
+  TokenKind kind;
   int value;
 } Token;
 
@@ -39,8 +55,26 @@ static inline void lexer_advance(Lexer* l) {
   l->current++;
 }
 
+static inline Token lexer_make_token(Lexer* l, TokenKind kind) {
+  // TODO: implement position spans
+  return (Token) {.kind = kind};
+}
+
+static inline bool is_white_space(char c) {
+  return (c == ' ' || c == '\t' || c == '\r');
+}
+
 Token lexer_next_token(Lexer* l) {
+  if (l->current == NULL || *l->current == '\0')
+    return lexer_make_token(l, TOKEN_END_OF_FILE);
+
+  LOG("Current char: '%c'\n", *l->current);
   switch (*l->current) {
+  case ' ':
+    while (is_white_space(*l->current)) lexer_advance(l);
+    return lexer_next_token(l);
+  case '+':
+    return lexer_make_token(l, TOKEN_PLUS);
   case '0':
   case '1':
   case '2':
@@ -54,9 +88,12 @@ Token lexer_next_token(Lexer* l) {
     lexer_backup(l);
     int number = *l->current - '0';
     while (isdigit(*(++l->current))) {
+      LOG("Current char: '%c'\n", *l->current);
       number = (number * 10) + (*l->current - '0');
       lexer_advance(l);
     }
+    l->current--; // don't skip a char after the number
+
     return (Token) {.kind = TOKEN_LITERAL_NUMBER, .value = number};
   default:
     if (g_raiz_error_buffer[0] != '\0') // means it is not empty
@@ -64,14 +101,28 @@ Token lexer_next_token(Lexer* l) {
 
     // Here we are sure that the buffer will not overflow
     sprintf(g_raiz_error_buffer, "unrecognized character: %c", *l->current);
-    return (Token) {.kind = TOKEN_ERROR };
+    return lexer_make_token(l, TOKEN_ERROR);
   }
 
-  return (Token) {.kind = TOKEN_END_OF_FILE};
+  return lexer_make_token(l, TOKEN_END_OF_FILE);
 }
 
 int main(void) {
-  // char code[] = "2 + 2";
+  char code[] = "2 + 2";
+  Lexer lexer = lexer_init(code);
+
+  Token next;
+  while ((next = lexer_next_token(&lexer)).kind != TOKEN_END_OF_FILE) {
+    if (next.kind == TOKEN_ERROR) {
+      LOG("%s\n", g_raiz_error_buffer);
+      return 1;
+    }
+
+    printf("Kind ID: %d", next.kind);
+    if (next.kind == TOKEN_LITERAL_NUMBER) printf(", value: %d", next.value);
+    printf("\n");
+    lexer_advance(&lexer);
+  }
 
   return 0;
 }
