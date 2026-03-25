7a42d7c3a14f5b2923c2e4b1c1629a8c8bbf91ca
Calebe Souza <cacalebehs@gmail.com>
2026-03-25 10:54:37 -0300

    build(nob): use better flags "parsing"

--- nob.c
+++ nob.c
@@ -7,10 +7,19 @@
 #define OUTPUT_PATH BUILD_FOLDER"raiz"
 #define CC_ARGS "-Wall", "-Wextra", "-ggdb"
 
-bool build_raiz(void)
+bool build_raiz(bool no_logs, bool strict)
 {
   Cmd cmd = {0};
   cmd_append(&cmd, "clang", "-o", OUTPUT_PATH, RAIZ_SOURCE, CC_ARGS);
+
+  if (no_logs)
+  {
+    cmd_append(&cmd, "-DNO_LOGS");
+  }
+  if (strict)
+  {
+    cmd_append(&cmd, "-Werror", "-pedantic", "-Wpedantic");
+  }
 
   if (!cmd_run(&cmd)) return false;
 
@@ -23,31 +32,45 @@ int main(int argc, char* argv[])
 
   if (!mkdir_if_not_exists(BUILD_FOLDER)) return 1;
 
-  if (!build_raiz()) return 2;
+  bool no_logs = false;
+  bool strict = false;
+  bool run_after = false;
+  bool debug = false;
 
   if (argc > 1)
   {
+    for (int i = 1; i < argc; i++)
+    {
+      if (strncmp(argv[i], "run", 3) == 0)
+      {
+        run_after = true;
+      }
+      else if (strncmp(argv[i], "debug", 5) == 0)
+      {
+        debug = true;
+      }
+      else if (strncmp(argv[i], "--strict", 8) == 0)
+      {
+        strict = true;
+      }
+      else if (strncmp(argv[i], "--no-logs", 9) == 0)
+      {
+        no_logs = true;
+      }
+    }
+  }
+  if (!build_raiz(no_logs, strict)) return 2;
+  if (run_after)
+  {
     Cmd cmd = {0};
-
-    if (strncmp(argv[1], "run", 3) == 0)
-    {
-      cmd_append(&cmd, BUILD_FOLDER"raiz");
-    }
-    else if (strncmp(argv[1], "debug", 5) == 0)
-    {
-      cmd_append(&cmd, "gdb", BUILD_FOLDER"raiz");
-    }
-    else if (strncmp(argv[1], "strict", 6) == 0)
-    {
-      TODO("build with strict flags");
-    }
-    else if (strncmp(argv[1], "--no-logs", 9) == 0)
-    {
-      cmd_append(&cmd, "clang", "-o", OUTPUT_PATH, RAIZ_SOURCE,
-                 CC_ARGS, "-DDISABLE_LOGS");
-    }
-
-    if (!cmd_run(&cmd)) return 1;
+    cmd_append(&cmd, OUTPUT_PATH);
+    if (!cmd_run(&cmd)) return 3;
+  }
+  else if (debug)
+  {
+    Cmd cmd = {0};
+    cmd_append(&cmd, "gdb", OUTPUT_PATH);
+    if (!cmd_run(&cmd)) return 3;
   }
   return 0;
 }
