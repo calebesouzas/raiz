60f4f7ffc749b55810f455209a0e5b52ffe278dc
Calebe Souza <cacalebehs@gmail.com>
2026-04-02 22:03:21 -0300

    move to Rust...

--- nob.c
+++ nob.c
@@ -2,79 +2,60 @@
 #define NOB_STRIP_PREFIX
 #include "nob.h"
 
-#define BUILD_FOLDER "build/"
-#define OBJECTS_FOLDER BUILD_FOLDER"/obj/"
+struct BuildFlags { bool run; bool strict; };
+struct BuildFlags build_flags = {0};
 
-#define RAIZ_SOURCE "raiz.c"
-#define BINARIES_FOLDER BUILD_FOLDER"/bin/"
-#define OUTPUT_PATH BINARIES_FOLDER"raiz"
-
-#define CC_ARGS "-Wall", "-Wextra", "-ggdb"
-
-bool build_raiz(bool no_logs, bool strict)
-{
-  Cmd cmd = {0};
-  cmd_append(&cmd, "clang", "-o", OUTPUT_PATH, RAIZ_SOURCE, CC_ARGS);
-
-  if (no_logs)
+bool
+build (void) { // i kinda like this formatting
+  if (build_flags.strict)
   {
-    cmd_append(&cmd, "-DRZ_DISABLE_LOGS");
-  }
-  if (strict)
-  {
-    cmd_append(&cmd, "-Werror", "-pedantic", "-Wpedantic");
+    Cmd cargo_clippy = {
+      .items = {"cargo", "clippy", NULL},
+      .count = 2,
+      .capacity = 2
+    };
+
+    // Ayo, optional parameters bro! Look at it!
+    if (!cmd_run(&cargo_clippy, .dont_reset = true)) return false;
   }
 
-  if (!cmd_run(&cmd)) return false;
+  if (build_flags.run)
+  {
+    Cmd cargo_run = {
+      .items = {"cargo", "run", NULL},
+      .count = 2,
+      .capacity = 2
+    };
+
+    if (!cmd_run(&cargo_run, .dont_reset = true)) return false;
+  }
+  else
+  {
+    Cmd cargo_build = {
+      .items = {"cargo", "build", NULL},
+      .count = 2,
+      .capacity = 2
+    };
+
+    if (!cmd_run(&cargo_build, .dont_reset = true)) return false;
+  }
 
   return true;
 }
 
-int main(int argc, char* argv[])
-{
+int
+main (int argc, char **argv) {
   GO_REBUILD_URSELF(argc, argv);
 
-  if (!mkdir_if_not_exists(BUILD_FOLDER)) return 1;
+  for (char *arg = NULL; (arg = *argv) != NULL; argv++)
+  { // can we omit these curly braces?
+    if (strncmp(arg, "run", 3) == 0) build_flags.run = true;
+    else if (strncmp(arg, "--strict", 8) == 0) build_flags.strict = true;
+    else fprintf(stderr, "nob.c: unknown argument: %s\n", arg);
+  }
 
-  bool no_logs = false;
-  bool strict = false;
-  bool run_after = false;
-  bool debug = false;
+  if (!build()) return -1; // for me it turns into 255, don't know why.
+                           // Looks like the OS uses 'uint8_t' for exit codes...
 
-  if (argc > 1)
-  {
-    for (int i = 1; i < argc; i++)
-    {
-      if (strncmp(argv[i], "run", 3) == 0)
-      {
-        run_after = true;
-      }
-      else if (strncmp(argv[i], "debug", 5) == 0)
-      {
-        debug = true;
-      }
-      else if (strncmp(argv[i], "--strict", 8) == 0)
-      {
-        strict = true;
-      }
-      else if (strncmp(argv[i], "--no-logs", 9) == 0)
-      {
-        no_logs = true;
-      }
-    }
-  }
-  if (!build_raiz(no_logs, strict)) return 2;
-  if (run_after)
-  {
-    Cmd cmd = {0};
-    cmd_append(&cmd, OUTPUT_PATH);
-    if (!cmd_run(&cmd)) return 3;
-  }
-  else if (debug)
-  {
-    Cmd cmd = {0};
-    cmd_append(&cmd, "gdb", OUTPUT_PATH);
-    if (!cmd_run(&cmd)) return 3;
-  }
   return 0;
 }
