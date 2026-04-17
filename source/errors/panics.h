2c1ea2abd43013a856dab2bd1ab0fe8e1ea17ee5
Calebe Souza <cacalebehs@gmail.com>
2026-04-17 09:02:08 -0300

    style(errors@panics): add missing stderr

--- source/errors/panics.h
+++ source/errors/panics.h
@@ -2,7 +2,10 @@
 
 #define RAIZ_PANIC(...)\
   do {\
-    fprintf("raiz: panicked at %s:%d:%s():\n", __FILE__,__LINE__,__func__);\
+    const char *file = __FILE__;\
+    const char *func = __func__;\
+    const int line = __LINE__;\
+    fprintf(stderr, "raiz: panicked at %s:%d:%s():\n", file, line, func);\
     fprintf(stderr, __VA_ARGS__);\
     abort();\
   } while (0)
