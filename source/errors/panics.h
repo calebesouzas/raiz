9efe210c8eea8d7401b0d79f85696ba58889b137
calebesouzas <cacalebehs@gmail.com>
2026-04-21 16:34:09 -0300

    doc(errors): add @doc comments

--- source/errors/panics.h
+++ source/errors/panics.h
@@ -1,5 +1,21 @@
 #pragma once
 
+/**
+ * @doc!
+ * @name errors/panics.h
+ * @type header
+ * @tags helper, errors, debugging
+ */
+
+/**
+ * @doc!
+ * @name PANIC
+ * @type macro
+ * @tags, helper, errors, panics
+ * @param `...` format string and arguments passed into `fprintf`
+ * @desc crashes the program after printing information about where the panic
+ * was called and the custom message from variadic argument list.
+ */
 #define RAIZ_PANIC(...)\
   do {\
     const char *file = __FILE__;\
@@ -10,4 +26,12 @@
     abort();\
   } while (0)
 
+/**
+ * @doc!
+ * @name TODO
+ * @type macro
+ * @tags helper, panics
+ * @param `message` custom simple message about what has to be done
+ * @desc calls `[@PANIC]` with "TODO: <message>\n"
+ */
 #define RAIZ_TODO(message) RAIZ_PANIC("TODO: %s\n", (message))
