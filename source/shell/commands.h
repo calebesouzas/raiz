d63ee7fddf3f599e68087d9a0152045d43ba88c9
Calebe Souza <cacalebehs@gmail.com>
2026-04-29 07:41:00 -0300

    doc(todo): add task to implement process spawning

--- source/shell/commands.h
+++ source/shell/commands.h
@@ -3,38 +3,6 @@
 #include <errno.h>
 #include <unistd.h>
 
-#define RAIZ_CMD_RUN_UNIX_VA(command, ...)\
-  do\
-  {\
-    char *__cmd = (command);\
-    pid_t pid = fork();\
-    if (pid == 0)\
-    {\
-      execlp(__cmd, __cmd, __VA_ARGS__, (char*)NULL);\
-    }\
-    else if (pid < 0)\
-    {\
-      RAIZ_PANIC("failed to spawn child process: %s. %s\n",\
-          __cmd, strerror(errno));\
-    }\
-  } while (0)
-
-#define RAIZ_CMD_RUN_UNIX_VEC(command, argv)\
-  do\
-  {\
-    char *__cmd = (command);\
-    pid_t pid = fork();\
-    if (pid == 0)\
-    {\
-      execvp(__cmd, (argv));\
-    }\
-    else if (pid < 0)\
-    {\
-      RAIZ_PANIC("failed to spawn child process: %s. %s\n",\
-          __cmd, strerror(errno));\
-    }\
-  } while (0)
-
 typedef struct
 {
   char **argv;
@@ -46,3 +14,12 @@ bool raiz_cmd_run(Raiz_Cmd *command);
 
 #define raiz_cmd_add(cmd, ...) raiz_cmd_add_((cmd), __VA_ARGS__, (char*)NULL)
 void raiz_cmd_add_(Raiz_Cmd *command, ...);
+
+/*@todo
+ * Implement wrappers for subprocess (command) spawning.
+ * We must have all basic features, like:
+ * - [ ] redirecting stdin, stdout and stderr;
+ * - [ ] getting exit or signal status and exit code if exited "normally";
+ * - [ ] choose between running asyncronously or not;
+ * - [ ] piping multiple commands (passing one's stdout into other's stdin);
+ */
