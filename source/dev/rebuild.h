3209fb0d81757f986249ea3b41f2a67a7b43ef10
calebesouzas <cacalebehs@gmail.com>
2026-05-01 14:37:55 -0300

    chore(rebuild): include needed headers

--- source/dev/rebuild.h
+++ source/dev/rebuild.h
@@ -1,13 +1,14 @@
 #pragma once
 
+#include <sys/wait.h> // waitpid()
+
 #include "common/libc.h"
-#include "debug/logs.h"
-#include "filesystem/file.h"
-#include "filesystem/time.h"
-#include "shell/commands.h"
+#include "debug/logs.h" // RAIZ_LOG
+#include "filesystem/file.h" // file_read()
+#include "filesystem/time.h" // get_<...>_time()
 #include "strings/builder.h"
 
 /*@doc! usage
 Call it and the Raiz project will rebuild itself if needed.
 */
-void raiz_rebuild_urself(int argc, char **argv);
+void raiz_rebuild_urself(char **argv);
