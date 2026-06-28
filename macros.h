b53f78443a3af18e672143738f6c612645ccf4a1
calebesouzas <cacalebehs@gmail.com>
2026-06-28 15:35:40 -0300

    Refactor panicking macros

--- macros.h
+++ macros.h
@@ -3,24 +3,24 @@
 
 #define PANIC(...)\
   do {\
-    fprintf(stderr, "internal panic at %s:%s:%zu\n",\
-            __FILE__, __func__, __LINE__);\
+    fprintf(stderr, "internal panic at %s:%d %s()\n",\
+            __FILE__, __LINE__, __FUNCTION__);\
     fprintf(stderr, __VA_ARGS__);\
     exit(-1);\
   } while (0)
 
 #define TODO(...)\
   do {\
-    fprintf(stderr, "internal TODO at %s:%s:%zu\n",\
-            __FILE__, __func__, __LINE__);\
+    fprintf(stderr, "internal TODO at %s:%d %s()\n",\
+            __FILE__, __LINE__, __FUNCTION__);\
     fprintf(stderr, __VA_ARGS__);\
     exit(-1);\
   } while (0)
 
 #define UNREACHABLE(...)\
   do {\
-    fprintf(stderr, "reached internal unreachable code at %s:%s:%zu\n",\
-            __FILE__, __func__, __LINE__);\
+    fprintf(stderr, "reached internal unreachable code at %s:%d %s()\n",\
+            __FILE__, __LINE__, __FUNCTION__);\
     fprintf(stderr, __VA_ARGS__);\
     exit(-1);\
   } while (0)
