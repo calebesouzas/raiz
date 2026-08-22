70741d318cf50cfa68bc1cb8c692c3c30640d1bb
Calebe Souza <cacalebehs@gmail.com>
2026-08-22 10:56:44 -0300

    Refactor `Type` declaration and related macros

--- types.h
+++ types.h
@@ -2,11 +2,11 @@
 #define RAIZ_TYPES_H
 
 #define TYPES_BUILTIN\
-  X(int, 4),\
-  X(uint, 4),\
-  X(char, 1),\
-  X(byte, 1),\
-  X(bool, 1),\
+  X(int, 4)\
+  X(uint, 4)\
+  X(char, 1)\
+  X(byte, 1)\
+  X(bool, 1)\
   X(string, 0)
 
 typedef struct Type {
@@ -15,19 +15,18 @@ typedef struct Type {
     size_t len;
   } name;
   enum {
-    #define X(typename, typesize) TYPE_##typename,
+    #define X(typename, typesize) TYPE_ ## typename,
     TYPES_BUILTIN
     #undef X
   } kind;
+  size_t size; // 0 for unknown
 } Type;
 
 #define g_TYPE(typename, typesize)\
 const Type g_TYPE_##typename = (Type){\
   .name = {.str = #typename, .len = sizeof(#typename)},\
-  .kind = TYPE_##typename,\
-  .data = {\
-    .size = typesize,\
-  },\
+  .kind = TYPE_ ## typename,\
+  .size = (typesize),\
 };
 
 #define X g_TYPE
